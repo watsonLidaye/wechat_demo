f66e34852b284b3d87c884f2a3b6d82c05c8d651
watsonLidaye <610466535@qq.com>
2018-11-12 10:09:26 +0800

    update

--- src/components/pages/my/myIntro/index.vue
+++ src/components/pages/my/myIntro/index.vue
@@ -1,101 +1,57 @@
 <template>
   <div>
-    <!-- panel -->
-    <div class="user_panel">
-      <!-- hd -->
-      <div class="user_hd mb15">
-        <!-- user -->
-        <div class="user_info">
-          <div>
-            <h3 class="user_name mb25">{{name}}<img v-if="sex === 1"
-                   class="icon_30 ml15"
-                   src="./image/ico_nan@2x.png"><img v-else
-                   class="icon_30 ml15"
-                   src="./image/ico_nv@2x.png"></h3>
-            <router-link to="/myInfo">
-              <p class="mine_info">我的信息<img class="icon_30 ml10"
-                     src="./image/_icobj@2x.png"></p>
-            </router-link>
+    <div class="intro_panel">
+      <div class="intro_list">
+        <router-link to="/introDetail">
+          <div class="intro_item bg_red">
+            <div class="color_f8f8f8">
+              <div class="mb35">
+                <img src="http://upload.jianshu.io/users/upload_avatars/3343569/3cd46650-54bc-4383-989f-80aacb0c42fe.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/180/h/180"
+                     alt=""
+                     class="intro_avatar">
+                <div class="inline_block">
+                  <h3 class="ft32 font_weight">王五</h3>
+                  <span class="intro_status color_red">已入职</span>
+                </div>
+              </div>
+              <p class="ft24">入职公司：富士康</p>
+            </div>
+            <div class="flex_v_center"><img class="high_price"
+                   src="./image/推荐200@2x.png"
+                   alt=""></div>
           </div>
-          <img class="user_avatar"
-               :src="headimgurl">
-        </div>
-      </div>
-      <!-- mian -->
-      <div class="user_main">
-        <!-- operation -->
-        <div class="user_operation">
-          <router-link to="/myWork">
-            <div class="operation_item">
-              <img class="icon_60 mr25"
-                   src="./image/ico_work@2x.png">
-              <div class="w_100 operation_item_r border_e2e2e2">
-                我的工作<img class="icon_30"
-                     src="./image/arrow_1@2x.png">
+        </router-link>
+        <router-link to="/introDetail">
+          <div class="intro_item bg_blue">
+            <div class="color_f8f8f8">
+              <div class="mb35">
+                <img src="http://upload.jianshu.io/users/upload_avatars/3343569/3cd46650-54bc-4383-989f-80aacb0c42fe.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/180/h/180"
+                     alt=""
+                     class="intro_avatar">
+                <div class="inline_block">
+                  <h3 class="ft32 font_weight">王五</h3>
+                  <span class="intro_status color_blue">已入职</span>
+                </div>
               </div>
+              <p class="ft24">入职公司：富士康</p>
             </div>
-          </router-link>
-          <router-link to="/myIntro">
-            <div class="operation_item">
-              <img class="icon_60 mr25"
-                   src="./image/ico_tuijian@2x.png">
-              <div class="w_100 operation_item_r border_e2e2e2">
-                我的推荐<img class="icon_30"
-                     src="./image/arrow_1@2x.png">
-              </div>
-            </div>
-          </router-link>
-          <router-link to="/myAccount">
-            <div class="operation_item mb15">
-              <img class="icon_60 mr25"
-                   src="./image/ico_zhanghu@2x.png">
-              <div class="w_100 operation_item_r">
-                我的账户<img class="icon_30"
-                     src="./image/arrow_1@2x.png">
-              </div>
-            </div>
-          </router-link>
-          <router-link to="/myBorrow">
-            <div class="operation_item">
-              <img class="icon_60 mr25"
-                   src="./image/ico_jieqian@2x.png">
-              <div class="w_100 operation_item_r border_e2e2e2">
-                我要借钱<img class="icon_30"
-                     src="./image/arrow_1@2x.png">
-              </div>
-            </div>
-          </router-link>
-          <div class="operation_item">
-            <img class="icon_60 mr25"
-                 src="./image/ico_kefu@2x.png">
-            <div class="w_100 operation_item_r">
-              联系客服<img class="icon_30"
-                   src="./image/arrow_1@2x.png">
-            </div>
+            <div class="flex_v_center"><img class="high_price"
+                   src="./image/推荐200@2x.png"
+                   alt=""></div>
           </div>
-        </div>
+        </router-link>
       </div>
     </div>
   </div>
 </template>
-<script type='text/ecmascript-6'>
+<script>
 export default {
-  name: 'my',
+  name: 'myIntro',
   data () {
-    return {
-      id: '',
-      name: 'Leslie Cheung',
-      sex: 2,
-      headimgurl: 'https://avatars2.githubusercontent.com/u/39576364?s=460&v=4'
-    }
+    return {}
   },
   created () { },
-  mounted () {
-    // console.log(this.$route)
-    // this.$router.push({
-    //   path: '/mine/mineInfo'
-    // })
-  },
+  mounted () { },
   methods: {},
   computed: {},
   destroyed () { }
